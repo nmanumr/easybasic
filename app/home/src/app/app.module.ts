bc86c3525beaaae2e7caa4ae543e00e936ced06f
Nauman Umer <Nauman Umer>
2017-02-25 13:09:36 +0500

    updated home

--- app/home/src/app/app.module.ts
+++ app/home/src/app/app.module.ts
@@ -1,25 +1,17 @@
-import { docbarComponent } from './layout/doc-bar/doc-bar.component';
-import { ConsoleService } from './services/console.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AngularSplitModule } from 'angular-split';
 import { AppComponent } from './app.component';
-import { ExpansionPanelsModule } from 'ng2-expansion-panels';
 import { MaterialModule } from '@angular/material';
 import 'hammerjs';
 import { TitleBarComponent } from './layout/title-bar/title-bar.component';
-import { TabsService } from './services/tabs.service';
-import { ConsoleComponent } from './console/console/console.component';
-import { StatusbarComponent } from './layout/statusbar/statusbar.component'
+import { StatusbarComponent } from './layout/statusbar/statusbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleBarComponent,
-    docbarComponent,
-    ConsoleComponent,
     StatusbarComponent
   ],
   imports: [
@@ -27,10 +19,8 @@ import { StatusbarComponent } from './layout/statusbar/statusbar.component'
     FormsModule,
     HttpModule,
     MaterialModule.forRoot(),
-    AngularSplitModule,
-    ExpansionPanelsModule
   ],
-  providers: [TabsService, ConsoleService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- app/home/src/app/services/tabs.ts
+++
@@ -1,6 +0,0 @@
-export class tab {
-  title: string;
-  isCloseable: boolean;
-  data: string;
-  active: boolean;
-}
