98d4638b7b9c16832603e29e028dd30d5bed8eeb
Nauman Umer <u.nauman@outlook.com>
2017-03-22 19:54:09 +0500

    added BASIC colors

---
+++ app/shell/src/app/services/consoleServices/colors.ts
@@ -0,0 +1,209 @@
+export class basicColors {
+
+    private _screen: 'text' | 'cga' | 'ega';
+    private _palette: '0' | '1' | 'alt';
+    /**
+     * Constructor for BASIC colours class
+     * @param mood specify screen mood e.g. text, cga, ega
+     * @param palette specify palette for screen 1 e.g. 0, 1, alt
+     */
+    constructor(mood: 'text' | 'cga' | 'ega', palette?: '0' | '1' | 'alt') {
+        this._screen = mood;
+        this._palette = palette;
+    }
+
+    /**
+     * Returns EGA or CGA color code from BASIC color number (only for screen 1-9)
+     * @param index BASIC color number
+     */
+    public getColor(index: number): string {
+        switch (this._screen) {
+            case 'cga':
+                if (this._palette == 'alt') {
+                    return this.cgaAlt[index];
+                } else if (this._palette == '1') {
+                    return this.cga1[index];
+                } else {
+                    return this.cga0[index];
+                }
+            case 'ega':
+                this._egaCustom[index];
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * Returns text forecolor from BASIC forecolor number (only for screen 0)
+     * @param index BASIC color number
+     */
+    public getForecolor(index: number): string {
+        if(this._screen == 'text'){
+            return this.textForecolor[index];
+        }
+    }
+
+    /**
+     * Returns text backcolor from BASIC backcolor number (only for screen 0)
+     * @param index BASIC color number
+     */
+    public getBackcolor(index: number): string {
+        if(this._screen == 'text'){
+            return this.textBackcolor[index];
+        }
+    }
+
+     /**
+     * change ega Palette
+     * @param index BASIC color number
+     * @param color BASIC number of new Color;
+     */
+    public changeEgaColour(index: number, color: number) {
+        if(this._screen == 'cga'){
+            if(index < 64 && index > -1)
+                this._egaCustom[index] = this.egaAll[color];
+            else
+                throw "EGA color index out of range"
+        }
+    }
+
+    private textBackcolor = [
+        '#000000',
+        '#0000aa',
+        '#00aa00',
+        '#00aaaa',
+        '#aa0000',
+        '#aa00aa',
+        '#aa5500',
+        '#aaaaaa'
+    ]
+
+    private textForecolor = [
+        '#000000',
+        '#0000aa',
+        '#00aa00',
+        '#00aaaa',
+        '#aa0000',
+        '#aa00aa',
+        '#aa5500',
+        '#aaaaaa',
+        '#555555',
+        '#5555ff',
+        '#55ff55',
+        '#55ffff',
+        '#ff5555',
+        '#ff55ff',
+        '#ffff55',
+        '#ffffff',
+    ]
+
+    private cga0 = [
+        '#000000',
+        '#55ff55',
+        '#ff5555',
+        '#aa5500'
+    ]
+
+    private cga1 = [
+        '#000000',
+        '#aaffff',
+        '#ff55ff',
+        '#ffffff'
+    ]
+
+    private cgaAlt = [
+        '#000000',
+        '#aaffff',
+        '#ff5555',
+        '#ffffff'
+    ]
+
+    private egaDefault = [
+        '#000000',
+        '#0000aa',
+        '#00aa00',
+        '#00aaaa',
+        '#aa0000',
+        '#aa00aa',
+        '#aa5500',
+        '#aaaaaa',
+        '#555555',
+        '#5555ff',
+        '#55ff55',
+        '#55ffff',
+        '#ff5555',
+        '#ff55ff',
+        '#ffff55',
+        '#ffffff'
+    ]
+
+    private _egaCustom = JSON.parse(JSON.stringify(this.egaDefault));
+
+    private egaAll = [
+        '#000000',
+        '#000055',
+        '#005500',
+        '#005555',
+        '#550000',
+        '#550055',
+        '#555500',
+        '#555555',
+        '#0000aa',
+        '#0000ff',
+        '#0055aa',
+        '#0055ff',
+        '#5500aa',
+        '#5500ff',
+        '#5555aa',
+        '#5555ff',
+        '#00aa00',
+        '#00aa55',
+        '#00ff00',
+        '#00ff55',
+        '#55aa00',
+        '#55aa55',
+        '#55ff00',
+        '#55ff55',
+        '#00aaaa',
+        '#00aaff',
+        '#00ffaa',
+        '#00ffff',
+        '#55aaaa',
+        '#55aaff',
+        '#55ffaa',
+        '#55ffff',
+        '#aa0000',
+        '#aa0055',
+        '#aa5500',
+        '#aa5555',
+        '#ff0000',
+        '#ff0055',
+        '#ff5500',
+        '#ff5555',
+        '#aa00aa',
+        '#aa00ff',
+        '#aa55aa',
+        '#aa55ff',
+        '#ff00aa',
+        '#ff00ff',
+        '#ff55aa',
+        '#ff55ff',
+        '#aaaa00',
+        '#aaaa55',
+        '#aaff00',
+        '#aaff55',
+        '#ffaa00',
+        '#ffaa55',
+        '#ffff00',
+        '#ffff55',
+        '#aaaaaa',
+        '#aaaaff',
+        '#aaffaa',
+        '#aaffff',
+        '#ffaaaa',
+        '#ffaaff',
+        '#ffffaa',
+        '#ffffff'
+    ]
+
+}
